a0ed56b1c169f189a6446294d13ab0fb33307683
a132395 <67187730+a132395@users.noreply.github.com>
2020-07-05 11:42:24 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+function handler()
+{
 curl -LO https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz
 tar xvf subconverter_linux64.tar.gz
 mv -f subconverter/subconverter subconverter/subconverter.exe
+}
